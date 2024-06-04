70e843c8f314b64dc84a54ba4dbd87439ebe2664
joshks <43095030+joshks@users.noreply.github.com>
2024-06-04 18:03:38 -0400

    Adding ARIMA model

---
+++ models/arima_model.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+
+class ARIMAModel:
+    def __init__(self, order):
+        self.order = order
+
+    def fit(self, X):
+        self.model = ARIMA(X, order=self.order).fit()
+
+    def predict(self, start, end):
+        return self.model.predict(start=start, end=end)
+
