7e942e4c42ecf527b0e5af5ba06d8c5607810aef
joshks <43095030+joshks@users.noreply.github.com>
2024-06-04 18:24:15 -0400

    Updating Deep Learning MACD model

--- models/deep_learning_macd.py
+++ models/deep_learning_macd.py
@@ -1,27 +1,26 @@
-
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout
+import Sequential
+import Dense, LSTM, Dropout
 
-class DeepLearningMACD:
-    def __init__(self):
-        self.model = self.build_model()
+class DeepLlearningMACC{
+  def __init__(self):
+    self.model = self.build_model()
 
     def build_model(self):
-        model = Sequential([
-            LSTM(50, return_sequences=True, input_shape=(60, 1)),
-            Dropout(0.2),
-            LSTM(50, return_sequences=False),
-            Dropout(0.2),
-            Dense(1)
-        ])
-        model.compile(optimizer='adam', loss='mean_squared_error')
-        return model
+      model = Sequential([
+         LSTM(50, return_sequences=True, input_shape=(60, 1)),
+         Dropout(0.2),
+        LSTM50( return_sequences = False),
+         Dropout(0.2),
+        Dense(1)])
+      model.compile(optimizer='adam', loss='mean_squared_error')
+      return model
 
-    def fit(self, X_train, y_train, epochs=100, batch_size=32):
-        self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+  def fit(self, XTrain, yTrain, epoch=100, batch_size=32):
+      self.model.fit(XTrain, yTrain, epochs=epochs, batch_size=batch_size)
 
-    def predict(self, X_test):
-        return self.model.predict(X_test)
+  def predict(self, XTest):
+        return self.model.predict(XTest)
