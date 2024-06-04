c209f28bb961c72e73cbca4d8a3d152d8f3e367b
joshks <43095030+joshks@users.noreply.github.com>
2024-06-04 17:57:01 -0400

    Adding Kalman filter model

---
+++ models/kalman_filter_model.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+import pandas as pd
+from pykalman import KalmanFilter
+
+class KalmanFilterPairsTrading:
+    def __init__(self):
+        self.kf = KalmanFilter(n_dim_obs=2, n_dim_state=2)
+
+    def fit(self, X):
+        self.kf = self.kf.em(X, n_iter=100)
+
+    def predict(self, X):
+        state_means, _ = self.kf.filter(X)
+        return state_means
+
+    def get_hedge_ratio(self, X):
+        state_means = self.predict(X)
+        return state_means[:, 0]
